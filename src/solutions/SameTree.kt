eaba52f111e35135f176b6bbe54a872caf142e4f
Murzin Andrey <MVMurzin@sberbank.ru>
2021-08-20 18:28:56 +0300

    solution for "Same Tree"

---
+++ src/solutions/SameTree.kt
@@ -0,0 +1,52 @@
+package solutions
+
+import main.model.TreeNode
+import java.util.*
+
+class SameTree {
+
+    fun isSameTree(p: TreeNode?, q: TreeNode?): Boolean {
+        val pQueue : Queue<TreeNode> = LinkedList()
+        val qQueue : Queue<TreeNode> = LinkedList()
+
+        p?.let(pQueue::offer)
+        q?.let(qQueue::offer)
+
+        while(pQueue.isNotEmpty() || qQueue.isNotEmpty()) {
+            val pSize = pQueue.size
+            val qSize = qQueue.size
+
+            if(pSize != qSize) {
+                return false
+            }
+
+            for(i in 0 until pSize) {
+                val pTree = pQueue.poll()
+                val qTree = qQueue.poll()
+
+                if(pTree.`val` != qTree.`val`) {
+                    return false
+                }
+
+                val pLeft = pTree?.left
+                val pRight = pTree?.right
+
+                val qLeft = qTree?.left
+                val qRight = qTree?.right
+
+                if(pLeft?.`val` != qLeft?.`val` || pRight?.`val` != qRight?.`val`) {
+                    return false
+                }
+
+                pLeft?.let(pQueue::offer)
+                pRight?.let(pQueue::offer)
+
+                qLeft?.let(qQueue::offer)
+                qRight?.let(qQueue::offer)
+
+            }
+        }
+
+        return true
+    }
+}
