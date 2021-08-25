41b65cf7ab39c3ea6e7854881ab799740cad06a0
Murzin Andrey <MVMurzin@sberbank.ru>
2021-08-25 17:39:18 +0300

    solution for "Invert binary tree"

---
+++ src/solutions/InvertBinaryTree.kt
@@ -0,0 +1,20 @@
+package solutions
+
+import main.model.TreeNode
+
+class InvertBinaryTree {
+
+    fun invertTree(node: TreeNode?): TreeNode? {
+        if(node == null) return null
+
+        val temp = node.right
+
+        node.right = node.left
+        node.left = temp
+
+        invertTree(node.left)
+        invertTree(node.right)
+
+        return node
+    }
+}
